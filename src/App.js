4d94dacc19bb52460b3e6bd4d9fcf8fb1b9b8cc1
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-02-02 09:39:03 -0600

    styeles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import './styles/main.scss';
 import Header from './components/Header';
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,17 +4,39 @@ import requests from '../requests';
 function Nav({ setSelectedOption }) {
   return (
     <div className="nav">
-      <h2 onClick={() => setSelectedOption(requests.fetchTrending)}>Trending</h2>
-      <h2 onClick={() => setSelectedOption(requests.fetchTopRated)}>Top Rated</h2>
-      <h2 onClick={() => setSelectedOption(requests.fetchActionMovies)}>Action</h2>
-      <h2 onClick={() => setSelectedOption(requests.fetchComedyMovies)}>Comedy</h2>
-      <h2 onClick={() => setSelectedOption(requests.fetchHorrorMovies)}>Horror</h2>
-      <h2 onClick={() => setSelectedOption(requests.fetchRomanceMovies)}>Romance</h2>
-      <h2 onClick={() => setSelectedOption(requests.fetchMystery)}>Mystery</h2>
-      <h2 onClick={() => setSelectedOption(requests.fetchScifi)}>Sci-fi</h2>
-      <h2 onClick={() => setSelectedOption(requests.fetchWestern)}>Western</h2>
-      <h2 onClick={() => setSelectedOption(requests.fetchActionMovies)}>Animation</h2>
-      <h2 onClick={() => setSelectedOption(requests.fetchTV)}>TV Movie</h2>
+      <div className="nav__item">
+        <h2 onClick={() => setSelectedOption(requests.fetchTrending)}>Trending</h2>
+      </div>
+      <div className="nav__item">
+        <h2 onClick={() => setSelectedOption(requests.fetchTopRated)}>Top Rated</h2>
+      </div>
+      <div className="nav__item">
+        <h2 onClick={() => setSelectedOption(requests.fetchActionMovies)}>Action</h2>
+      </div>
+      <div className="nav__item">
+        <h2 onClick={() => setSelectedOption(requests.fetchComedyMovies)}>Comedy</h2>
+      </div>
+      <div className="nav__item">
+        <h2 onClick={() => setSelectedOption(requests.fetchHorrorMovies)}>Horror</h2>
+      </div>
+      <div className="nav__item">
+        <h2 onClick={() => setSelectedOption(requests.fetchRomanceMovies)}>Romance</h2>
+      </div>
+      <div className="nav__item">
+        <h2 onClick={() => setSelectedOption(requests.fetchMystery)}>Mystery</h2>
+      </div>
+      <div className="nav__item">
+        <h2 onClick={() => setSelectedOption(requests.fetchScifi)}>Sci-fi</h2>
+      </div>
+      <div className="nav__item">
+        <h2 onClick={() => setSelectedOption(requests.fetchWestern)}>Western</h2>
+      </div>
+      <div className="nav__item">
+        <h2 onClick={() => setSelectedOption(requests.fetchActionMovies)}>Animation</h2>
+      </div>
+      <div className="nav__item">
+        <h2 onClick={() => setSelectedOption(requests.fetchTV)}>TV Movie</h2>
+      </div>
     </div>
   )
 }
