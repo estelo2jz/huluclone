22e227fe15ffb4be72066fffbb65848533a0dce8
esteloabell <manitobaaa@gamil.com>
2021-02-02 20:50:23 -0600

    Update TVAdultAnimationData.js

--- src/components/Category/CategoryData/TVData/TVAdultAnimationData.js
+++ src/components/Category/CategoryData/TVData/TVAdultAnimationData.js
@@ -13,19 +13,19 @@ export const TVAdultAnimationData = [
   {
     image: "https://deltastatement.com/wp-content/uploads/2018/10/family-guy.jpg",
     path: "/tv",
-    title: "American Dad",
+    title: "Family Guy",
     rated: "TV14",
     genre: "Comedy, Animation",
-    year: "(2012)",
+    year: "(2009)",
     series: "TV Series"
   },
   {
     image: "https://bloximages.newyork1.vip.townnews.com/redandblack.com/content/tncms/assets/v3/editorial/6/79/6793490c-0828-11e2-b884-0019bb30f31a/524b78e645991.image.jpg",
     path: "/tv",
-    title: "American Dad",
+    title: "Brickleberry",
     rated: "TV14",
     genre: "Comedy, Animation",
-    year: "(2012)",
+    year: "(2019)",
     series: "TV Series"
   },
   {
