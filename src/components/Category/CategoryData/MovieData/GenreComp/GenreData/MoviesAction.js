76505917af9c501246eea90b60e790ce2f558333
esteloabell <manitobaaa@gamil.com>
2021-02-05 16:23:58 -0600

    styles

--- src/components/Category/CategoryData/MovieData/GenreComp/GenreData/MoviesAction.js
+++ src/components/Category/CategoryData/MovieData/GenreComp/GenreData/MoviesAction.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const ActionMovieData = [
   {
     image: "https://img1.looper.com/img/uploads/2019/04/John-Wick.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "John Wick 3",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -12,7 +12,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://m.media-amazon.com/images/M/MV5BOTdkNGY5YjItOWU5OC00NTA0LTkxMGEtZDRlNWZmMmI2ZGZmXkEyXkFqcGdeQXVyODIwMDI1NjM@._V1_.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Action",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -21,7 +21,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://www.englishclub.com/images/vocabulary/movies/pusher.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "PUSHER",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -30,7 +30,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://cms-assets.tutsplus.com/uploads/users/71/posts/14079/final_image/final_large3.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Vendetta",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -39,7 +39,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Black Panther",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -48,7 +48,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://i.ytimg.com/vi/TuBfY8phrJU/maxresdefault.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Action Hero",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -57,7 +57,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/action-movie-poster-template-design-0f5fff6262fdefb855e3a9a3f0fdd361_screen.jpg?ts=1573101130",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "FREEDOM",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -66,7 +66,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://lh3.googleusercontent.com/proxy/vcWJ43b58_6i_KW8KF2x5BwI9Tteh3PYpl9hJ1T9_Tag_g1vP6UpAiKUb__4_NTwgUrq8eXf7yTmat6bL2qcEWbJPkNTgcrewMsjvo1o12EVnFKkxRBms4NvTI5DK-t_LHbJdhrB0d5-SdTue7lJwp0gmzHm",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Drive",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -75,7 +75,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://lh3.googleusercontent.com/proxy/zZ-op_NejksnM_AfbTX0lwpacUQar1eUX-YDRobcfPRydZ4L9nBnXP73fXd3lZJ8zXYwe_EK24SklJ5qcycxk8-Vo4x-NIv5gM8SOxEQFyXvyT_QTsKfjjc",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Dhoom 3",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -84,7 +84,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://upload.wikimedia.org/wikipedia/en/thumb/1/14/Tenet_movie_poster.jpg/220px-Tenet_movie_poster.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Tenet",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -93,7 +93,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://wallpapercave.com/wp/UeJv6hp.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "ERAGON",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -102,7 +102,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTISkf8UMObW8emiYUVLsK0JAhju9Lo1NMTIw&usqp=CAU",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Lord of the Rings",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -111,7 +111,7 @@ export const ActionMovieData = [
   },
   {
     image: "http://yeinjee.com/wp-content/uploads/2014/11/attack-titan-2.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Attack on Titan",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -119,8 +119,8 @@ export const ActionMovieData = [
     series: "Movie"
   },
   {
-    image: "https://crimsonnews.org/wp-content/uploads/2016/02/Maleficent.jpg",
-    path: "/movie/action",
+    image: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/87424224024957.5632cceadc9cb.jpg",
+    path: "/movies/action",
     title: "Maleficent",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -129,7 +129,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIMQTTIb3GM5ISBRW2-K-agnl2He-jVHMNsA&usqp=CAU",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Fast & Furious",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -138,7 +138,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://www.japantrends.com/japan-trends/wp-content/uploads/2014/11/attack-on-titan-shingeki-no-kyojin-film-movie-live-action-poster-main-300x259.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Shikishima",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -147,7 +147,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://gizmostory.com/wp-content/uploads/2020/12/The-Bourne-Identity.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Bourne Identity",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -156,7 +156,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://bleedingcool.com/wp-content/uploads/2017/11/FantasticFourPoster.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Fantastic 4",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -165,7 +165,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://www.liveabout.com/thmb/5rMNqIcJCiK6DAXJBZN9fy7sUdA=/1320x894/filters:no_upscale():max_bytes(150000):strip_icc()/johnwick-f38cdfe056cd40f2a6ea4e3928fc8a53.png",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "John Wick",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -174,7 +174,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://fourcoloursandthetruth.files.wordpress.com/2011/09/attack-the-block-movie-poster.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Attack The Block",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -183,7 +183,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://imgix.ranker.com/user_node_img/110/2186618/original/the-dark-knight-films-photo-u19?fit=crop&fm=pjpg&q=60&w=144&h=144&dpr=2",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Batman",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -192,7 +192,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://graphicriver.img.customer.envatousercontent.com/files/167430289/presentation.jpg?auto=compress%2Cformat&fit=crop&crop=top&w=590&h=590&s=aa8de2a276c4593a41583158b275e7ec",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Technical",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -201,7 +201,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Movie_poster_i_robot.jpg/220px-Movie_poster_i_robot.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "I.ROBOT",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -210,7 +210,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://static2.showtimes.com/poster/160x236/godzilla-vs-kong-150409.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "GODZILLA v. KONG",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -219,7 +219,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://m.media-amazon.com/images/M/MV5BMjE1ODM3MTI5MF5BMl5BanBnXkFtZTcwNjc3MDcxNQ@@._V1_.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Tekken",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -228,7 +228,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://d2j1wkp1bavyfs.cloudfront.net/image-assets/558574/nQbd4u1NrvcXUbfnmCQYxkF5GIC.jpg?d=360x540&q=50",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Echo Boomers",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -237,7 +237,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://graphicriver.img.customer.envatousercontent.com/files/146533694/presentation.jpg?auto=compress%2Cformat&fit=crop&crop=top&w=590&h=590&s=2200a4716d3772139e804ecbb20fd32b",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Zombie Apocalypse",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -246,7 +246,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://i.ytimg.com/vi/aHbUkniW9CM/maxresdefault.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "SAAYA",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -255,7 +255,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://lh3.googleusercontent.com/proxy/FCC_RGvSpbQ7kMM4FZDPL7i13oHgBoVXhqKPkQwQ_xyHYN6VFGcCtw3F2TlMTipXgJ5MhPN3UfW7UQ5YOesEA6suBxkbRZKIb1uzEvyysD-3bGptOpEoGWR7z5aDH4O9IySr42FthABGZcnPnhpi6j-hKcgSvdixz7OW574",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "The 300",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -264,7 +264,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://lh3.googleusercontent.com/proxy/aW4JnhCS47en_WJnXhVjYkvbn8VbxmYSIwtQW7YYjsh77_f-TMikNDPvvPErXK9PaCd1r-dPATPZS8xydmhP0n_G-cPfFbzuSJY_wgLhb6PEUfJVxA",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Kill Bill",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -273,7 +273,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://static.rogerebert.com/uploads/movie/movie_poster/20-2018/widget_20-poster.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Reloaded",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -282,7 +282,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGJEZE2da1Ak3jxrAMfcgEpM2Ej7G2xeZSMw&usqp=CAU",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Thor",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -291,7 +291,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://media.santabanta.com/medium1/hollywood%20movies/vanguard/vanguard-0a.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Jackie Chan",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -300,7 +300,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://i.pinimg.com/originals/f1/c9/2a/f1c92a12ba266ee92f02652288360803.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Transporter 3",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -309,7 +309,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS34LvFZ6BBkxBUgLLrpzXL_J-0wTO1cZJ9qQ&usqp=CAU",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Avengers Infinity War",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -318,7 +318,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLaVKq8xM0FGahPrW0vQlQvAd9pt9B2LPMdg&usqp=CAU",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Outcast",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -327,7 +327,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://c8.alamy.com/comp/D5K2DJ/liam-neeson-poster-the-grey-2012-D5K2DJ.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "The Grey",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -336,7 +336,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://i.pinimg.com/originals/72/9a/35/729a35bfdc6a5fa9cb97ae3a3668a96d.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Urban Justice",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -345,7 +345,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://i.pinimg.com/originals/01/eb/51/01eb513d05fd0f43c57f80bdf96d022c.png",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Rescuer",
     rated: "TVMA",
     genre: "Comedy, Animation",
@@ -354,7 +354,7 @@ export const ActionMovieData = [
   },
   {
     image: "https://images-na.ssl-images-amazon.com/images/I/51E4xdHPdQL._AC_SX466_.jpg",
-    path: "/movie/action",
+    path: "/movies/action",
     title: "Beowulf",
     rated: "TVMA",
     genre: "Comedy, Animation",
---
+++ src/components/Category/CategoryData/MovieData/GenreComp/GenreData/MoviesNews.js
@@ -0,0 +1,434 @@
+export const NewsMovieData = [
+  {
+    image: "http://www.essentialkids.com.au/content/dam/images/g/k/p/d/b/u/image.gallery.galleryLandscape.620x414.gkpe40.png/1446609264472.jpg",
+    path: "/movies/adultanimation",
+    title: "Journey to the Center of the Earth",
+    rated: "TVMA",
+    genre: "Comedy, Animation",
+    year: "(2012)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+  {
+    image: "",
+    path: "/movies/adultanimation",
+    title: "",
+    rated: "TVMA",
+    genre: "Action, Adventure",
+    year: "(2020)",
+    series: "Movie"
+  },
+]
